cf7b2375dec7d97800df4c2dbaa9a3b0cd52d6a6
Caot22 <caot2204@gmail.com>
2025-05-23 10:07:52 -0600

    First commit

---
+++ src/electron/main.ts
@@ -0,0 +1,24 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { isDev } from './util.js';
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  if (isDev()) {
+    mainWindow.loadURL('http://localhost:5123');
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), 'dist-react/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow()
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+});
